061666f152728f3b46ede13f36130a662544c321
annadrewnewby <anewby@unomaha.edu>
2020-02-27 18:21:02 -0600

    minor change

--- mfscrm/manage.py
+++ mfscrm/manage.py
@@ -12,7 +12,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from execute_from_command_line(sys.argv)
+        )from execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
