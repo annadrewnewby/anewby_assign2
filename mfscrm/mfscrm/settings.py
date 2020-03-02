b91505969d3247c51bec8d1938f1edee170268ab
annadrewnewby <anewby@unomaha.edu>
2020-03-02 16:52:15 -0600

    .gitignore file created

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b^sm&$w9&56+f8m5id*5f(00)7r$k1tl=f6hny=x%52cu2(($('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,3 +126,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
